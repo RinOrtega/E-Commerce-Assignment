e2ef7914dafc9731364ecc78b447deb4ef4cf60b
ortegaerin@icloud.com <ortegaerin@icloud.com>
2024-04-25 00:22:01 -0500

    Updated README.md

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -58,7 +58,7 @@ router.put('/:id', async (req, res) => {
 
 router.delete('/:id', async (req, res) => {
   try {
-    const catData = await Category.destroy(req.body, {
+    const catData = await Category.destroy( {
 
       where: {
         id: req.params.id,

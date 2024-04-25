7f4dd20ba5514eb42ac7a5a157f7557899bf6604
ortegaerin@icloud.com <ortegaerin@icloud.com>
2024-04-25 00:11:19 -0500

    Updated Code Incomplete

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -7,6 +7,20 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+
+    // defines the category, and establishes primary key
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+
+    // defines the column for the category name
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
   },
   {
     sequelize,
--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -7,6 +7,28 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    // defines the id for the prodcut, is being referenced from the product model.
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id'
+      }
+    },
+    // NEED TO DEFINE TAG_ID COLUMN
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
